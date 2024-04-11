0f04b49679957abc940e8e5d4bcd789929a8caa7
BannovDaniil <baddan@mail.ru>
2024-04-11 15:53:06 +0300

    security off for test

--- src/main/java/ru/greenatom/forum/configuration/SecurityConfig.java
+++ src/main/java/ru/greenatom/forum/configuration/SecurityConfig.java
@@ -2,17 +2,33 @@ package ru.greenatom.forum.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/**").anonymous()
+                ).formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        return http.build();
+    }
+
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails admin = User.builder()
--- src/main/java/ru/greenatom/forum/controller/ForumController.java
+++ src/main/java/ru/greenatom/forum/controller/ForumController.java
@@ -1,6 +1,7 @@
 package ru.greenatom.forum.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.greenatom.forum.model.Topic;
 import ru.greenatom.forum.model.dto.TopicIncomingDto;
@@ -19,22 +20,25 @@ public class ForumController {
     private final TopicService topicService;
 
     @PostMapping("/topic")
-
+    @PreAuthorize("hasAuthority('ROLE_USER') && hasAuthority('ROLE_ADMIN')")
     public Topic createTopic(@RequestBody TopicIncomingDto topicIncomingDto) {
         return new Topic();
     }
 
     @PutMapping("/topic")
+    @PreAuthorize("hasAuthority('ROLE_USER') && hasAuthority('ROLE_ADMIN')")
     public Topic updateTopic(@RequestBody TopicUpdateDto topicUpdateDto) {
         return new Topic();
     }
 
     @GetMapping("/topic")
+    @PreAuthorize("hasAuthority('ROLE_USER') && hasAuthority('ROLE_ADMIN')")
     public List<TopicOutDto> getTopic() {
         return topicService.findAll();
     }
 
     @GetMapping("/topic/{uuid}")
+    @PreAuthorize("hasAuthority('ROLE_USER') && hasAuthority('ROLE_ADMIN')")
     public TopicOutFullDto getTopic(@PathVariable(name = "uuid") UUID uuid) {
 //        UUID topicUuid = UUID.fromString(uuid);
         return topicService.findById(uuid);
